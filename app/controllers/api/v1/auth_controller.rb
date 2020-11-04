2bdba3a8b7e6cd8a643122c8c30a67a72ed196e5
John Nguyen <john@techfox.io>
2020-11-04 17:40:53 +0700

    fix bug

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,25 +2,29 @@ module Api
   module V1
     class AuthController < ApplicationController
       def login
-        user = User.find_by(username: params[:username])
-        
-        if user.try(:authenticate, params[:password])
+        user = User.find_by(username: normalize_params[:username])
+
+        if user.try(:authenticate, normalize_params[:password])
           json_response({
               access_token: user.try(:token),
-              user: user.try(:json_builder)
+              user: user.try(:json_builder),
+              message: 'Đăng nhập thành công!'
             },
-            'Đăng nhập thành công!',
             :ok
           )
 
           return
         end
 
-        json_response({}, 'Sai tài khoản hoặc mật khẩu!', 401)
+        json_response({ error: { message: 'Sai tài khoản hoặc mật khẩu!' } }, 401)
       end
 
       def forgot_password
         
+      end
+
+      def normalize_params
+        params.require(:auth).permit(:username, :password)
       end
     end
   end
