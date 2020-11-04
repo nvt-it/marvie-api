05fd2d842f7ffb607945d571c80a1214e69bfd66
John Nguyen <john@techfox.io>
2020-11-04 16:10:09 +0700

    fix puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,37 +1,35 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+# frozen_string_literal: true
+# #!/usr/bin/env puma
+# # frozen_string_literal: true
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+directory '/root/apps/cargo-api/current'
+rackup '/root/apps/cargo-api/current/config.ru'
+environment 'production'
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+tag ''
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile '/root/apps/cargo-api/shared/tmp/pids/puma.pid'
+state_path '/root/apps/cargo-api/shared/tmp/pids/puma.state'
+stdout_redirect '/root/apps/cargo-api/shared/log/puma_access.log', '/root/apps/cargo-api/shared/log/puma_error.log', true
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads 4, 16
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+bind 'unix:///root/apps/cargo-api/shared/tmp/sockets/cargo-api-puma.sock'
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+workers 0
+preload_app!
+
+on_restart do
+  puts 'Refreshing Gemfile'
+  ENV['BUNDLE_GEMFILE'] = ''
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
